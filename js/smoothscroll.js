53bdb19b7984d4018775984f6a9180b396f83053
M-Mike <ratsimbazafy.louis@gmail.com>
2016-05-14 15:02:45 -0300

    Add smoothscroll
    ajout des id jumbotron et footer pour la reconnaissance du scrollsmooth
    de la partie à aller
    ajout du scrollsmooth.js et du script dans la partie javascript du code
    html
    modification de la classe navbar-brand pour avoir la référence du
    jumbotron, comme ça on peut scroller dedans quand on clique sur le
    bouton

---
+++ js/smoothscroll.js
@@ -0,0 +1,44 @@
+//Permet d'aller smoothly vers la section recherchée en cliquant sur l'onglet
+
+//Pour descendre aux sections concernées
+$(".navbar-collapse ul li a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top -50
+     }, 700, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
+//Pour revenir à l'icône onemillionlines
+$("a.navbar-brand[href^='#']").on('click', function(e) {
+//a = lien -> on a fait un lien vers navbar-brand
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top -50
+     }, 700, function(){
+      //-50 est un offset, pour régler la position lors du click
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
